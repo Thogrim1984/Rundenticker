5f1aab6676e523b8a926a693cea5e04296255ec9
Thogrim <thogrim@online.de>
2021-10-21 01:03:59 +0200

    replace step 3-2

--- phases/income/step2.php
+++ phases/income/step2.php
@@ -1,32 +1,23 @@
 <h2>Schritt 2: Einlagern</h2>
 
-<div data-activatable="yes" id="step_3">
+<div>
     <form>
-        <label for="3_2_depositBpCount">Wieviele BP werden durch Einlagern generiert?</label>
-        <input id="3_2_depositBpCount" type="number" min="0" value="0"> <br>
-        <button type="button" name="3_2_resultButton">Weiter</button>
+        <label for="step_3_2_depositBpCount">Wieviele BP werden durch Einlagern generiert? <input id="step_3_2_depositBpCount" type="number" min="0" value="0"></label><br />
+        <button type="button" name="resultButton" id="step_3_2_resultButton">Weiter</button>
     </form>
 </div>
 
-<div data-activatable="yes" id="step_4">
-    Ergebnis:<br>
-    <span id="3_2_result"></span><br>
-    <button type="button" name="next_step" value="4" onclick="load_next_step(this.value)">Weiter</button>
-</div>
-
 <script>
-    $(document).ready(function() {
-        $("button[name='3_2_resultButton']").click(function() {
+    $("#step_3_2_resultButton").click(function() {
 
-            var bpCost = Number($('#3_2_depositBpCount').val());
+        var bp = $('#step_3_2_depositBpCount').val();
 
-            var resultText = [];
-            if (bpCost > 0) {
-                resultText.push(getResult([getResultObject("bp", bpCost)]));
-            } else {
-                resultText = getResult([getResultObject("nothing", 0)]);
-            }
-            $('#3_2_result').html(resultText.join("<br />"));
-        });
+        var resultText = [];
+        if (bp > 0) {
+            resultText.push(getResult([getResultObject("bp", updateActualBp(bp))]));
+        } else {
+            resultText = getResult([getResultObject("nothing", 0)]);
+        }
+        nextStep(resultText.join("<br />"), 3, 2);
     });
 </script>
