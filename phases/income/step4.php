23abb330f756ddb63955846ba2628bf7bc792d66
Thogrim <thogrim@online.de>
2021-10-21 01:18:23 +0200

    refactor step 3-4

--- phases/income/step4.php
+++ phases/income/step4.php
@@ -1,39 +1,30 @@
 <h2>Schritt 4: Steuern eintreiben</h2>
 
-<div data-activatable="yes" id="step_3">
+<div>
     <form>
-        <label for="3_4_collectTaxes">Wie hoch ist der WIR-Wurf?</label>
-        <input id="3_4_collectTaxes" type="number" value="0"> <br>
-        <input type="checkbox" name="" id="3_4_collectTaxesControlDc"> <label for="">Wurde der Kontroll-SG erreicht?</label>
-    <button type="button" name="3_4_resultButton">Weiter</button>
+        <label for="step_3_4_collectTaxes">Wie hoch ist der WIR-Wurf? <input id="step_3_4_collectTaxes" type="number" value="0"></label> <br />
+        <label><input type="checkbox" id="step_3_4_collectTaxesControlDc"> Wurde der Kontroll-SG erreicht?</label><br />
+        <button type="button" name="resultButton" id="step_3_4_resultButton">Weiter</button>
     </form>
 </div>
 
-<div data-activatable="yes" id="step_4">
-    Ergebnis:<br>    
-    <span id="3_4_result"></span><br>
-    <button type="button" name="next_step" value="4" onclick="load_next_step(this.value)">Weiter</button>
-</div>
-
 <script>
-    $(document).ready(function() {
-        $("button[name='3_4_resultButton']").click(function() {
+    $("#step_3_4_resultButton").click(function() {
 
-            var bpCost =  Number($('#3_4_collectTaxes').val());
+        var bp = $('#step_3_4_collectTaxes').val();
 
-            if ($("#3_4_collectTaxesControlDc").is(':checked')) {
-                bpCost /=  3;
-            } else {
-                bpCost /=  4;
-            }
+        if ($("#step_3_4_collectTaxesControlDc").is(':checked')) {
+            bp /= 3;
+        } else {
+            bp /= 4;
+        }
 
-            var resultText = [];
-            if (bpCost > 0) {
-                resultText.push(getResult([getResultObject("bp", bpCost)]));
-            } else {
-                resultText = getResult([getResultObject("nothing", 0)]);
-            }
-            $('#3_4_result').html(resultText.join("<br />"));
-        });
+        var resultText = [];
+        if (bp > 0) {
+            resultText.push(getResult([getResultObject("bp", updateActualBp(bp))]));
+        } else {
+            resultText = getResult([getResultObject("nothing", 0)]);
+        }
+        nextStep(resultText.join("<br />"), 3, 4);
     });
 </script>
