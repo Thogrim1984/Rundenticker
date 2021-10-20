6ff7e96a2ac8ad4e8828ce0419886ecc39515e85
Thogrim <thogrim@online.de>
2021-10-21 00:12:46 +0200

    refactor step 5-5

--- phases/edict/step5.php
+++ phases/edict/step5.php
@@ -1,91 +1,81 @@
 <h2>Schritt 5: Militär</h2>
 
 
-<div data-activatable="yes" id="step_3">
+<div>
     <form>
-        <fieldset id="2_5_militaryFieldset" class="activated">
-            <input type="checkbox" id="2_5_trainUnits"> <label for="2_5_trainUnits">Einheiten ausbilden: BP ausgegeben</label> <input id="2_5_trainUnitsCost" type="number" min="0" value="0"> <br>
-            <input type="checkbox" id="2_5_recruitUnits"> <label for="2_5_recruitUnits">Einheiten rekrutieren: BP ausgegeben</label> <input id="2_5_recruitUnitsCost" type="number" min="0" value="0"> <br>
-            <input type="checkbox" id="2_5_retrainUnits"> <label for="2_5_retrainUnits">Einheiten umschulen: BP ausgegeben</label> <input id="2_5_retrainUnitsCost" type="number" min="0" value="0"> <br>
-            <input type="checkbox" id="2_5_disbandUnits"> <label for="">Einheiten auflösen: Stabilitätswurf gelungen?</label> <br>
-            <fieldset id="2_5_disbandUnitsFieldset" class="activated">
-                <input type="radio" name="2_5_disbandUnits" id="2_5_disbandUnitsSuccess"> <label for="2_5_disbandUnitsSuccess">Erfolg: Wie viele Einheiten?</label> <input id="2_5_disbandUnitsSuccessCount" type="number" min="0" value="0"> <br>
-                <input type="radio" name="2_5_disbandUnits" id="2_5_disbandUnitsFailure"> <label for="2_5_disbandUnitsFailure">Misserfolg: 1-4 Punkte daneben</label><br>
-                <input type="radio" name="2_5_disbandUnits" id="2_5_disbandUnitsFumble"> <label for="2_5_disbandUnitsFumble">Misserfolg: 5+ Punkte daneben</label><br>
+        <fieldset id="step_2_5_militaryFieldset" class="activated">
+            <label for="step_2_5_trainUnits"><input type="checkbox" id="step_2_5_trainUnits"> Einheiten ausbilden: BP ausgegeben <input id="step_2_5_trainUnitsCost" type="number" min="0" value="0"></label> <br />
+            <label for="step_2_5_recruitUnits"><input type="checkbox" id="step_2_5_recruitUnits"> Einheiten rekrutieren: BP ausgegeben <input id="step_2_5_recruitUnitsCost" type="number" min="0" value="0"></label> <br />
+            <label for="step_2_5_retrainUnits"><input type="checkbox" id="step_2_5_retrainUnits"> Einheiten umschulen: BP ausgegeben <input id="step_2_5_retrainUnitsCost" type="number" min="0" value="0"></label> <br />
+            <label for=""><input type="checkbox" id="step_2_5_disbandUnits"> Einheiten auflösen: Stabilitätswurf gelungen?</label> <br />
+            <fieldset id="step_2_5_disbandUnitsFieldset" class="activated">
+                <label for="step_2_5_disbandUnitsSuccess"><input type="radio" name="step_2_5_disbandUnits" id="step_2_5_disbandUnitsSuccess"> Erfolg: Wie viele Einheiten? <input id="step_2_5_disbandUnitsSuccessCount" type="number" min="0" value="0"></label> <br />
+                <label for="step_2_5_disbandUnitsFailure"><input type="radio" name="step_2_5_disbandUnits" id="step_2_5_disbandUnitsFailure"> Misserfolg: 1-4 Punkte daneben</label><br />
+                <label for="step_2_5_disbandUnitsFumble"><input type="radio" name="step_2_5_disbandUnits" id="step_2_5_disbandUnitsFumble"> Misserfolg: 5+ Punkte daneben</label><br />
             </fieldset>
-            <input type="checkbox" id="2_5_trainCommanders"> <label for="2_5_trainCommanders">Befehlshaber ausbilden: BP ausgegeben</label> <input id="2_5_trainCommandersCost" type="number" min="0" value="0"> <br>
-            <input type="checkbox" id="2_5_xpStandby"> <label for="2_5_xpStandby">Erfahrung für Einheiten in Bereitschaft verteilen.</label> <br>
-            <input type="checkbox" id="2_5_recruitMercenaries"> <label for="2_5_recruitMercenaries">Söldner rekrutieren: BP ausgegeben</label> <input id="2_5_recruitMercenariesCost" type="number" min="0" value="0"> <br>
-            <input type="checkbox" id="2_5_combineUnits"> <label for="2_5_combineUnits">Einheiten zusammenschließen.</label> <br>
-            <input type="checkbox" id="2_5_splitUnits"> <label for="2_5_splitUnits">Einheiten aufteilen.</label> <br>
-            <input type="checkbox" id="2_5_reformUnit"> <label for="2_5_reformUnit">Einheiten neuformieren.</label> <br>
-            <input type="checkbox" id="2_5_buyEquipment"> <label for="2_5_buyEquipment">Ausrüstung kaufen: BP ausgegeben</label> <input id="2_5_buyEquipmentCost" type="number" min="0" value="0"> <br>
-            <input type="checkbox" id="2_5_equipUnits"> <label for="2_5_equipUnits">Einheiten ausrüsten.</label> <br>
-            <input type="checkbox" id="2_5_transferEuipment"> <label for="2_5_transferEuipment">Ausrüstung transferieren.</label> <br>
-            <input type="checkbox" id="2_5_splitEquipment"> <label for="2_5_splitEquipment">Ausrüstung aufteilen.</label> <br>
+            <label for="step_2_5_trainCommanders"><input type="checkbox" id="step_2_5_trainCommanders"> Befehlshaber ausbilden: BP ausgegeben <input id="step_2_5_trainCommandersCost" type="number" min="0" value="0"></label> <br />
+            <label for="step_2_5_xpStandby"><input type="checkbox" id="step_2_5_xpStandby"> Erfahrung für Einheiten in Bereitschaft verteilen.</label> <br />
+            <label for="step_2_5_recruitMercenaries"><input type="checkbox" id="step_2_5_recruitMercenaries"> Söldner rekrutieren: BP ausgegeben <input id="step_2_5_recruitMercenariesCost" type="number" min="0" value="0"></label> <br />
+            <label for="step_2_5_combineUnits"><input type="checkbox" id="step_2_5_combineUnits"> Einheiten zusammenschließen.</label> <br />
+            <label for="step_2_5_splitUnits"><input type="checkbox" id="step_2_5_splitUnits"> Einheiten aufteilen.</label> <br />
+            <label for="step_2_5_reformUnit"><input type="checkbox" id="step_2_5_reformUnit"> Einheiten neuformieren.</label> <br />
+            <label for="step_2_5_buyEquipment"><input type="checkbox" id="step_2_5_buyEquipment"> Ausrüstung kaufen: BP ausgegeben <input id="step_2_5_buyEquipmentCost" type="number" min="0" value="0"></label> <br />
+            <label for="step_2_5_equipUnits"><input type="checkbox" id="step_2_5_equipUnits"> Einheiten ausrüsten.</label> <br />
+            <label for="step_2_5_transferEuipment"><input type="checkbox" id="step_2_5_transferEuipment"> Ausrüstung transferieren.</label> <br />
+            <label for="step_2_5_splitEquipment"><input type="checkbox" id="step_2_5_splitEquipment"> Ausrüstung aufteilen.</label> <br />
         </fieldset>
-        <input type="checkbox" id="2_5_noChanges"> <label for="">Keine Änderungen.</label> <br>
-        <button type="button" name="2_5_resultButton">Weiter</button>
+        <label><input type="checkbox" id="step_2_5_noChanges"> Keine Änderungen.</label> <br />
+        <button type="button" name="resultButton" id="step_2_5_resultButton">Weiter</button>
     </form>
 </div>
 
-<div data-activatable="yes" id="step_4">
-    Ergebnis:<br>
-    <span id="2_5_result"></span>
-    <span id="2-5-result_bp" class="hiddenResult">BP ändern auf <span id="2-5-result_bp_number">[Nummer]</span>.</span>
-    <span id="2-5-keine_aenderung" class="hiddenResult">Keine Änderungen nötig.</span>
-    <button type="button" name="next_step" value="4" onclick="load_next_step(this.value)">Weiter</button>
-</div>
-
 <script>
-    $(document).ready(function() {
-        $("button[name='2_5_resultButton']").click(function() {
+    $("#step_2_5_resultButton").click(function() {
 
-            var bpCost = Number($('#2_5_trainUnitsCost').val()) + Number($('#2_5_recruitUnitsCost').val()) + Number($('#2_5_retrainUnitsCost').val()) + Number($('#2_5_trainCommandersCost').val()) + Number($('#2_5_recruitMercenariesCost').val()) + Number($('#2_5_buyEquipmentCost').val());
-            var noChanges = true;
+        var bpCost = $('#step_2_5_trainUnitsCost').val() + $('#step_2_5_recruitUnitsCost').val() + $('#step_2_5_retrainUnitsCost').val() + $('#step_2_5_trainCommandersCost').val() + $('#step_2_5_recruitMercenariesCost').val() + $('#step_2_5_buyEquipmentCost').val();
+        var noChanges = true;
 
 
-            var resultText = [];
-            if ($("#2_5_noChanges").is(':checked')) {
-                resultText.push('Denk dran, die Erfahrungspunkte/Aufstiege für Einheiten in Bereitschaft einzutragen.');
-            } else {
-                if (bpCost > 0) {
-                    resultText.push(getResult([getResultObject("bp", bpCost)]));
-                    noChanges = false;
-                }
-                if ($("#2_5_disbandUnitsSuccess").is(':checked')) {
-                    resultText.push('Ereignis eintragen: Arbeitskräfte durch aufgelöste Einheiten - +' + (Number($('#2_5_disbandUnitsSuccessCount').val()) * 2) + ' WIR.');
-                    noChanges = false;
-                }
-                if ($("#2_5_disbandUnitsFailure").is(':checked')) {
-                    resultText.push(getResult([getResultObject("unrest", 1)]));
-                    noChanges = false;
-                }
-                if ($("#2_5_disbandUnitsFumble").is(':checked')) {
-                    resultText.push(getResult([getResultObject("unrest", getDiceRoll(4))]));
-                    noChanges = false;
-                }
-                if (noChanges) {
-                    resultText = getResult([getResultObject("nothing", 0)]);
-                }
+        var resultText = [];
+        if ($("#step_2_5_noChanges").is(':checked')) {
+            resultText.push('Denk dran, die Erfahrungspunkte/Aufstiege für Einheiten in Bereitschaft einzutragen.');
+        } else {
+            if (bpCost > 0) {
+                resultText.push(getResult([getResultObject("bp", updateActualBp(bpCost))]));
+                noChanges = false;
             }
-            $('#2_5_result').html(resultText.join("<br />"));
-        });
-
-        $("input[id=2_5_noChanges]").change(function() {
-            if (this.checked) {
-                $("#2_5_militaryFieldset").toggleClass('activated deactivated');
-            } else {
-                $("#2_5_militaryFieldset").toggleClass('deactivated activated');
+            if ($("#step_2_5_disbandUnitsSuccess").is(':checked')) {
+                resultText.push('Ereignis eintragen: Arbeitskräfte durch aufgelöste Einheiten - +' + ($('#step_2_5_disbandUnitsSuccessCount').val() * 2) + ' WIR.');
+                noChanges = false;
             }
-        });
-
-        $("input[id=2_5_disbandUnits]").change(function() {
-            if (this.checked) {
-                $("#2_5_disbandUnitsFieldset").toggleClass('activated deactivated');
-            } else {
-                $("#2_5_disbandUnitsFieldset").toggleClass('deactivated activated');
+            if ($("#step_2_5_disbandUnitsFailure").is(':checked')) {
+                resultText.push(getResult([getResultObject("unrest", updateActualUnrest(1))]));
+                noChanges = false;
             }
-        });
+            if ($("#step_2_5_disbandUnitsFumble").is(':checked')) {
+                resultText.push(getResult([getResultObject("unrest", updateActualUnrest(getDiceRoll(4)))]));
+                noChanges = false;
+            }
+            if (noChanges) {
+                resultText = getResult([getResultObject("nothing", 0)]);
+            }
+        }
+        nextStep(resultText.join("<br />", 2, 5));
+    });
+
+    $("input[id=step_2_5_noChanges]").change(function() {
+        if (this.checked) {
+            $("#step_2_5_militaryFieldset").toggleClass('activated deactivated');
+        } else {
+            $("#step_2_5_militaryFieldset").toggleClass('deactivated activated');
+        }
+    });
+
+    $("input[id=step_2_5_disbandUnits]").change(function() {
+        if (this.checked) {
+            $("#step_2_5_disbandUnitsFieldset").toggleClass('activated deactivated');
+        } else {
+            $("#step_2_5_disbandUnitsFieldset").toggleClass('deactivated activated');
+        }
     });
 </script>
